d59c8f6cc2a7ca2d396f082adc782f05683cfbba
Dallin <dallinpoulson@gmail.com>
2022-05-12 13:10:18 -0600

    footer credit

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ function App() {
   const drawThree = () => {
     discards = [0];
     setShowAllCards(false);
-    setVP(0)
-    setClaimedMissions([])
+    setVP(0);
+    setClaimedMissions([]);
 
     let tempCards = [];
     for (let i = 0; i < 3; i++) {
@@ -108,118 +108,144 @@ function App() {
   return (
     <div className={`${game}-background`}>
       <Container className="app">
-        <h1 className="text-center p-2 ">OnePageRules Mission Cards</h1>
-        <h4 className="text-center p-2">
-          Select a game and click the <em>Draw Three</em> button for a fresh
-          deck.
-        </h4>
-        <Row className="pt-4">
-          <Col xs="12" md="6" lg="4" xl="3">
-            <Form.Select
-              value={game}
-              onChange={(e) => {
-                setGame(e.target.value);
-              }}
-            >
-              {games.map((g) => {
-                return (
-                  <option key={g.id} value={g.id}>
-                    {g.name}
-                  </option>
-                );
-              })}
-            </Form.Select>
-          </Col>
-        </Row>
-        <Row className="">
-          <Col xs="12" md="6">
-            <Button
-              className={`my-2 ${game}-button`}
-              onClick={() => {
-                setShowThree(false);
-                setShowAllCards(!showAllCards);
-              }}
-            >
-              {showAllCards ? "HIDE ALL CARDS" : "SHOW ALL CARDS"}
-            </Button>
-            <Button
-              className={`m-2 ${game}-button`}
-              onClick={() => drawThree()}
-            >
-              Draw Three
-            </Button>
-          </Col>
-          <Col className={`${game}-text-gradient`} xs="12" md="6">
-            <b
-              id={game + "-vp-button-add"}
-              className={` ${game}-vp-button vp-button   mx-3 pointer`}
-              onClick={() => changeVP(1)}
-            >
-              +
-            </b>
-            <b className={`${game}-vp-button`}>{vp} VP</b>
-            <b
-              id={game + "-vp-button-subtract"}
-              className={` ${game}-vp-button  vp-button mx-3 pointer`}
-              onClick={() => changeVP(-1)}
-            >
-              -
-            </b>
-            <span onClick={() => setVP(0)} className="pointer hover-red">
-              Reset
-            </span>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            {discards.length > 1 &&
-              showThree &&
-              (discards.length > 33 ? (
-                <span>
-                  {discards.length - 1} discards. 0 remain in the deck.
-                </span>
-              ) : (
-                <span>
-                  {discards.length - 1} discards. {34 - discards.length} remain
-                  in the deck.
-                </span>
-              ))}
-          </Col>
-        </Row>
-        {showThree && threeCards.length > 0 ? (
-          <>
-            <Cards
-              changeVP={changeVP}
-              cards={threeCards}
-              game={game}
-              discard={discard}
-              claimedMissions={claimedMissions}
-              setClaimedMissions={setClaimedMissions}
-            />
-            {claimedMissions.length > 0 && (
-              <Container className="bg-light rounded-3 p-3">
-                <h4 className='text-dark'>
-                  <u>Claimed Objectives</u>
-                </h4>
-                {claimedMissions.length > 0 &&
-                  claimedMissions.map((cm,i) => {
-                    return (
-                      <ClaimedMission
-                        key={cm.desc + i}
-                        mission={cm}
-                        unClaim={unClaim}
-                        game={game}
-                      />
-                    );
-                  })}
-              </Container>
-            )}
-          </>
-        ) : (
-          showThree &&
-          "You ran out of cards. Click 'Draw Three' to start fresh."
-        )}
-        {showAllCards && <Cards game={game} cards={cards} />}
+        <main>
+          <h1 className="text-center p-2 ">OnePageRules Mission Cards</h1>
+          <h4 className="text-center p-2">
+            Select a game and click the <em>Draw Three</em> button for a fresh
+            deck.
+          </h4>
+          <Row className="pt-4">
+            <Col xs="12" md="6" lg="4" xl="3">
+              <Form.Select
+                value={game}
+                onChange={(e) => {
+                  setGame(e.target.value);
+                }}
+              >
+                {games.map((g) => {
+                  return (
+                    <option key={g.id} value={g.id}>
+                      {g.name}
+                    </option>
+                  );
+                })}
+              </Form.Select>
+            </Col>
+          </Row>
+          <Row className="">
+            <Col xs="12" md="6">
+              <Button
+                className={`my-2 ${game}-button`}
+                onClick={() => {
+                  setShowThree(false);
+                  setShowAllCards(!showAllCards);
+                }}
+              >
+                {showAllCards ? "HIDE ALL CARDS" : "SHOW ALL CARDS"}
+              </Button>
+              <Button
+                className={`m-2 ${game}-button`}
+                onClick={() => drawThree()}
+              >
+                Draw Three
+              </Button>
+            </Col>
+            <Col className={`${game}-text-gradient`} xs="12" md="6">
+              <b
+                id={game + "-vp-button-add"}
+                className={` ${game}-vp-button vp-button   mx-3 pointer`}
+                onClick={() => changeVP(1)}
+              >
+                +
+              </b>
+              <b className={`${game}-vp-button`}>{vp} VP</b>
+              <b
+                id={game + "-vp-button-subtract"}
+                className={` ${game}-vp-button  vp-button mx-3 pointer`}
+                onClick={() => changeVP(-1)}
+              >
+                -
+              </b>
+              <span onClick={() => setVP(0)} className="pointer hover-red">
+                Reset
+              </span>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              {discards.length > 1 &&
+                showThree &&
+                (discards.length > 33 ? (
+                  <span>
+                    {discards.length - 1} discards. 0 remain in the deck.
+                  </span>
+                ) : (
+                  <span>
+                    {discards.length - 1} discards. {34 - discards.length}{" "}
+                    remain in the deck.
+                  </span>
+                ))}
+            </Col>
+          </Row>
+          {showThree && threeCards.length > 0 ? (
+            <>
+              <Cards
+                changeVP={changeVP}
+                cards={threeCards}
+                game={game}
+                discard={discard}
+                claimedMissions={claimedMissions}
+                setClaimedMissions={setClaimedMissions}
+              />
+              {claimedMissions.length > 0 && (
+                <Container className="bg-light rounded-3 p-3">
+                  <h4 className="text-dark">
+                    <u>Claimed Objectives</u>
+                  </h4>
+                  {claimedMissions.length > 0 &&
+                    claimedMissions.map((cm, i) => {
+                      return (
+                        <ClaimedMission
+                          key={cm.desc + i}
+                          mission={cm}
+                          unClaim={unClaim}
+                          game={game}
+                        />
+                      );
+                    })}
+                </Container>
+              )}
+            </>
+          ) : (
+            showThree &&
+            "You ran out of cards. Click 'Draw Three' to start fresh."
+          )}
+          {showAllCards && <Cards game={game} cards={cards} />}
+        </main>
+        <footer className={`${game === 'gdf' && 'text-secondary ' }   text-center`}>
+          <h6>
+
+          Created by{" "}
+          <a href="https://github.com/dpouls" target="_blank" rel="noreferrer">
+            dpouls 
+          </a>
+          . {" "}
+          </h6>
+          <h6>
+
+          <span>
+            If you like 3D printing, check out my other site{" "}
+            <a
+              href="http://stlbundles.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              STLBundles.com
+            </a>{" "}
+            to keep up with all of the STL subscription releases.
+          </span>
+              </h6>
+        </footer>
       </Container>
     </div>
   );
